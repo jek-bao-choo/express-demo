8b9929680e3de98557347371718ae43e3f8f1be2
Jek <jerchoo@outlook.com>
2021-08-02 10:09:05 +0800

    feat: add version 1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Express Demo!')
+  res.send('Express Demo! v1')
 })
 
 app.listen(port, () => {
