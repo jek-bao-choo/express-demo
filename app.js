109694f3c229bb6be141079656485252a409ea8f
Jek Bao CHOO <jek.bao.choo@gmail.com>
2021-08-04 18:19:10 +0800

    Update app.js to v5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Express Demo! v4')
+  res.send('Express Demo! v5')
 })
 
 app.listen(port, () => {
