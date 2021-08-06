1c28cd8b724cff1bed74230d2a88bccad290d517
Jek Bao CHOO <jek.bao.choo@gmail.com>
2021-08-06 09:53:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Express Demo! v6')
+  res.send('Express Demo! v7')
 })
 
 app.listen(port, () => {
